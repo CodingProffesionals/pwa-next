d0b04955d5884096af42d38a90184f88b9f5436b
Udit <easyprograming1994@gmail.com>
2024-04-04 11:45:34 +0530

    online/offline operation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,25 @@
+'use client'
 import Image from "next/image";
-
+import { useEffect, useRef, useState } from 'react';
 export default function Home() {
+  const [isOnline, setIsOnline] = useState('/wifi-on.jpg');
+
+  useEffect(() => {
+
+   window.addEventListener("offline", (event) => {
+    setIsOnline("/wifi-off.svg");
+    console.log("The network connection has been lost.");
+  });
+  
+  window.addEventListener("online", (event) => {
+    setIsOnline("/wifi-on.jpg");
+    console.log("The network connection is present");
+  });
+
+  },[])
   return (
     <>
+      <Image src={isOnline} alt="" width="400" height="400"  />
       <img src="img_la.jpg" alt="LA" style={{width:"100%"}} />
 
       <div className="w3-container w3-center w3-teal">
