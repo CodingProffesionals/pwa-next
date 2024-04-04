528f9b0413f0c0be45a4515a59cbe9cbe48c1a5b
Udit <easyprograming1994@gmail.com>
2024-04-04 12:30:37 +0530

    online/offline operation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,27 +5,31 @@ export default function Home() {
   // const [isOnline, setIsOnline] = useState('/wifi-on.jpg');
   const imgref = useRef(null);
   useEffect(() => {
-   
+    const srcimg:any = document.getElementById('imgwifi');
+    console.log('addEventListener',srcimg.src);
+
    window.addEventListener("offline", (event) => {
     event.preventDefault();
     // setIsOnline("/wifi-off.svg");
     const domget:any = imgref
-    domget.current.src = "/wifi-off.svg"
+    srcimg.src = "/wifi-off.svg"
     console.log("The network connection has been lost.");
   });
   
   window.addEventListener("online", (event) => {
     event.preventDefault();
     // setIsOnline("/wifi-on.jpg");
+    
+    
     const domget:any = imgref
-    domget.current.src = "/wifi-on.jpg"
+    srcimg.src = "/wifi-on.jpg"
     console.log("The network connection is present");
   });
 
   },[])
   return (
     <>
-      <Image ref={imgref} src="/wifi-on.jpg" alt="" width="400" height="400"  />
+      <img ref={imgref} id="imgwifi" src="/wifi-on.jpg" alt="" width="400" height="400"  />
       <img src="img_la.jpg" alt="LA" style={{width:"100%"}} />
 
       <div className="w3-container w3-center w3-teal">
