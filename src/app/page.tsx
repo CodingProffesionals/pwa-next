05d9afadf13d70aae181a520fc4e038fa15e61b3
Udit <easyprograming1994@gmail.com>
2024-04-04 12:46:45 +0530

    online/offline operation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,34 +2,24 @@
 import Image from "next/image";
 import { useEffect, useRef, useState } from 'react';
 export default function Home() {
-  // const [isOnline, setIsOnline] = useState('/wifi-on.jpg');
-  const imgref = useRef(null);
+  const [isOnline, setIsOnline] = useState('/wifi-on.jpg');
+
   useEffect(() => {
-    const srcimg:any = imgref;
-    console.log('addEventListener',srcimg.src);
 
    window.addEventListener("offline", (event) => {
-    event.preventDefault();
-    // setIsOnline("/wifi-off.svg");
-    const domget:any = imgref
-    srcimg.src = "/wifi-off.svg"
+    setIsOnline("/wifi-off.svg");
     console.log("The network connection has been lost.");
   });
   
   window.addEventListener("online", (event) => {
-    event.preventDefault();
-    // setIsOnline("/wifi-on.jpg");
-    
-    
-    const domget:any = imgref
-    srcimg.src = "/wifi-on.jpg"
+    setIsOnline("/wifi-on.jpg");
     console.log("The network connection is present");
   });
 
   },[])
   return (
     <>
-      <img ref={imgref} id="imgwifi" src="/wifi-on.jpg" alt="" width="400" height="400"  />
+      <Image src={isOnline} alt="" width="400" height="400"  />
       <img src="img_la.jpg" alt="LA" style={{width:"100%"}} />
 
       <div className="w3-container w3-center w3-teal">
